baf2286e361777aa7ca3f7985e3498c8d3c8c473
Smith Chokshi <sm714486@dal.ca>
2023-07-23 17:10:09 -0300

    resolved issue relatede to refresh and redirection

--- Frontend/src/components/Login/login.js
+++ Frontend/src/components/Login/login.js
@@ -1,106 +1,142 @@
-import { useState, useEffect } from 'react';
-import { Button, Form, Input, Layout, theme, Checkbox } from 'antd';
-import { FacebookOutlined } from '@ant-design/icons';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import useSimpleReactValidator from '../../helpers/useReactSimpleValidator';
-import { login } from '../../redux/actions/authActions';
-import './login.css';
-import GlobalHeader from '../../shared/header';
+  import { useState, useEffect } from 'react';
+  import { Button, Form, Input, Layout, theme, Checkbox } from 'antd';
+  import { FacebookOutlined } from '@ant-design/icons';
+  import { FacebookLoginButton } from "react-social-login-buttons";
+  import FacebookLoginBtn from 'react-facebook-login';
+  import {OldSocialLogin as SocialLogin} from "react-social-login";
+  import { useDispatch } from 'react-redux';
+  import { useNavigate } from 'react-router-dom';
+  import useSimpleReactValidator from '../../helpers/useReactSimpleValidator';
+  import { login } from '../../redux/actions/authActions';
+  import './login.css';
+  import GlobalHeader from '../../shared/header';
 
-const { Content } = Layout;
+  const { Content } = Layout;
 
-const Login = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const Login = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+    const {
+      token: { colorBgContainer },
+    } = theme.useToken();
 
-  const [loading, setLoading] = useState(false);
-  const [fields, setFields] = useState({
-    email: null,
-    password: null,
-  });
+    const [loading, setLoading] = useState(false);
+    const [fields, setFields] = useState({
+      email: null,
+      password: null,
+    });
 
-  const [validator, setValidator] = useSimpleReactValidator();
+    const [validator, setValidator] = useSimpleReactValidator();
 
-  const handleChange = (e, field) => {
-    setFields(prev => ({
-      ...prev,
-      [field]: e.target.value,
-    }));
-  };
+    const handleChange = (e, field) => {
+      setFields(prev => ({
+        ...prev,
+        [field]: e.target.value,
+      }));
+    };
 
-  const handleSubmit = async () => {
-    setLoading(true);
-    if (validator.allValid()) {
-      await dispatch(login(fields));
-      setLoading(false);
-      navigate('/products');
-    } else {
-      setLoading(false);
-      validator.getErrorMessages();
-      setValidator(true);
-    }
-  };
+    const handleSubmit = async () => {
+      setLoading(true);
+      if (validator.allValid()) {
+        await dispatch(login(fields));
+        setLoading(false);
+        navigate('/products');
+      } else {
+        setLoading(false);
+        validator.getErrorMessages();
+        setValidator(true);
+      }
+    };
 
-  const facebook = () => {
-    window.open("http://localhost:5006/api/users/facebook", "_self");
-  };
+    const facebook = () => {
+      window.open("http://localhost:5006/api/users/facebook", "_self");
+    };
 
-  return (
-    <Layout>
-  {/*<GlobalHeader title={'Products'} />*/}
-  <Content>
-    <div className="login-page">
-      <div className="login-box">
-        <div className="illustration-wrapper">
-          <img src="https://cdn.sites.tapfiliate.com/tapfiliate.com/2023/04/5-winning-marketing-strategies-for-e-commerce-this-year-1.jpg" alt="Login" />
-        </div>
-        <Form className="login-form" name="login-form" initialValues={{ remember: true }} layout="vertical">
-          <p className="form-title">Login</p>
-          <div style={{ display: 'flex', justifyContent: 'center', fontSize:'15px', fontFamily:'sans-serif', fontWeight:'bold'}}>
-            <p>Doesn't have an account yet? <a href="/register">Sign Up</a>
-            </p>
+    const handleSocialLogin = (user, err) => {
+      console.log(user);
+      console.log(err);
+    };
+
+    return (
+      <Layout>
+    {/*<GlobalHeader title={'Products'} />*/}
+    <Content>
+      <div className="login-page">
+        <div className="login-box">
+          <div className="illustration-wrapper">
+            <img src="https://cdn.sites.tapfiliate.com/tapfiliate.com/2023/04/5-winning-marketing-strategies-for-e-commerce-this-year-1.jpg" alt="Login" />
           </div>
-          <Form.Item className="" label={ <span className="label">
-            <span className="required-asterisk">*</span> Email </span> } name="email" > <Input type="text" placeholder="Enter your Email" value={fields.email} onChange={e=> handleChange(e, 'email')} autoComplete="new-password" className="custom-input" /> <div className={validator.errorMessages.email ? 'error-message' : '' }> {validator.message('Email', fields.email, 'required|email')} </div>
-          </Form.Item>
-          <Form.Item label={ <span className="label">
-            <span className="required-asterisk">*</span> Password </span> } name="password" > <Input.Password placeholder="Enter your Password" value={fields.password} onChange={e=> handleChange(e, 'password')} autoComplete="new-password" className="custom-input" /> <div className={validator.errorMessages.password ? 'error-message' : '' }> {validator.message('Password', fields.password, 'required')} </div>
-          </Form.Item>
-          <Form.Item>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-              <div>
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                  <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-              </div>
-              <div>
-                <Form.Item>
-                  <a href="/forgot-password">Forgot Password?</a>
-                </Form.Item>
-              </div>
+          <Form className="login-form" name="login-form" initialValues={{ remember: true }} layout="vertical">
+            <p className="form-title">Login</p>
+            <div style={{ display: 'flex', justifyContent: 'center', fontSize:'15px', fontFamily:'sans-serif', fontWeight:'bold'}}>
+              <p>Doesn't have an account yet? <a href="/register">Sign Up</a>
+              </p>
             </div>
-          </Form.Item>
-          <Form.Item>
-            <Button className="login-form-button" type="primary" htmlType="submit" onClick={handleSubmit} loading={loading}> Log In </Button>
-          </Form.Item>
-          <div style={{ display: 'flex', justifyContent: 'center', fontSize:'16px', fontFamily:'sans-serif', fontWeight:'bold'}}>
-            <p>Or Log In with</p>
-          </div>
-          <Form.Item>
-            <Button className="facebook-form-button" type="primary" onClick={facebook} loading={loading}>
-              <FacebookOutlined style={{ fontSize: '20px' }} />Continue with Facebook
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item className="" label={ <span className="label">
+              <span className="required-asterisk">*</span> Email </span> } name="email" > <Input type="text" placeholder="Enter your Email" value={fields.email} onChange={e=> handleChange(e, 'email')} autoComplete="new-password" className="custom-input" /> <div className={validator.errorMessages.email ? 'error-message' : '' }> {validator.message('Email', fields.email, 'required|email')} </div>
+            </Form.Item>
+            <Form.Item label={ <span className="label">
+              <span className="required-asterisk">*</span> Password </span> } name="password" > <Input.Password placeholder="Enter your Password" value={fields.password} onChange={e=> handleChange(e, 'password')} autoComplete="new-password" className="custom-input" /> <div className={validator.errorMessages.password ? 'error-message' : '' }> {validator.message('Password', fields.password, 'required')} </div>
+            </Form.Item>
+            <Form.Item>
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <div>
+                  <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                  </Form.Item>
+                </div>
+                <div>
+                  <Form.Item>
+                    <a href="/forgot-password">Forgot Password?</a>
+                  </Form.Item>
+                </div>
+              </div>
+            </Form.Item>
+            <Form.Item>
+              <Button className="login-form-button" type="primary" htmlType="submit" onClick={handleSubmit} loading={loading}> Log In </Button>
+            </Form.Item>
+            <div style={{ display: 'flex', justifyContent: 'center', fontSize:'16px', fontFamily:'sans-serif', fontWeight:'bold'}}>
+              <p>Or Log In with</p>
+            </div>
+            <Form.Item>
+              <FacebookLoginBtn
+                  appId="1275850609703508"
+                  autoLoad={false}
+                  fields="name,email,picture"
+                  scope="public_profile,user_friends,user_actions.books"
+                  authType="reauthenticate"
+                  // onClick={componentClicked}
+                  callback={(res) => console.log(res)} />
+              {/*<SocialLogin*/}
+              {/*    provider="facebook"*/}
+              {/*  appId="1275850609703508"*/}
+              {/*  callback={handleSocialLogin}*/}
+              {/*>*/}
+              {/*  <FacebookLoginButton />*/}
+              {/*</SocialLogin>*/}
+
+
+
+
+              {/*<SocialLogin*/}
+              {/*    provider="facebook"*/}
+              {/*    appId="1275850609703508"*/}
+              {/*    callback={handleSocialLogin}*/}
+              {/*>*/}
+              {/*  <Button className="facebook-form-button" type="primary" loading={loading}>*/}
+              {/*    <FacebookOutlined style={{ fontSize: '20px' }} />Continue with Facebook*/}
+              {/*  </Button>*/}
+              {/*</SocialLogin>*/}
+              {/*<Button className="facebook-form-button" type="primary" onClick={facebook} loading={loading}>*/}
+              {/*  <FacebookOutlined style={{ fontSize: '20px' }} />Continue with Facebook*/}
+              {/*</Button>*/}
+            </Form.Item>
+          </Form>
+        </div>
       </div>
-    </div>
-  </Content>
-</Layout>
-  );
-};
-export default Login;
+    </Content>
+  </Layout>
+    );
+  };
+  export default Login;
