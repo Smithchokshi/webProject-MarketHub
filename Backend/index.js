a14cbdd8048d0c876a9be9a837635c4e05c324e8
Priyank Patel <pr371749@dal.ca>
2023-07-26 23:39:04 -0300

    Added: final changes

--- Backend/index.js
+++ Backend/index.js
@@ -119,7 +119,7 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 5006;
 
 server.listen(PORT, () => {
   console.log('Server is running', PORT);
