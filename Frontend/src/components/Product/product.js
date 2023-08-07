698e399baeb9a1434e46d225219a6752e1b6dde3
Smith Chokshi <sm714486@dal.ca>
2023-08-06 22:22:42 -0300

    removed static data in comments and improved api call logic for likes

--- Frontend/src/components/Product/product.js
+++ Frontend/src/components/Product/product.js
@@ -1,7 +1,6 @@
-
 import React, { useState, useEffect } from 'react';
 import { LikeOutlined, ShareAltOutlined, CommentOutlined } from '@ant-design/icons';
-import { Avatar, Card, Row, Col, Tooltip, Layout,Select } from 'antd';
+import { Avatar, Card, Row, Col, Tooltip, Layout, Select } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { notification } from 'antd';
@@ -25,7 +24,6 @@ const Product = () => {
   const dispatch = useDispatch();
 
   const getData = async () => {
-
     try {
       const res = await api(false).loadUser();
       setUserName(res.data.userData.name);
@@ -56,11 +54,11 @@ const Product = () => {
     }
   };
 
-  const handleSearchResults = (results) => {
+  const handleSearchResults = results => {
     setCardData(results);
     setOriginalData(results);
   };
-  const extractPriceValue = (priceString) => {
+  const extractPriceValue = priceString => {
     const priceWithoutCurrency = priceString ? priceString.replace(/\D+/g, '') : '0';
     return parseInt(priceWithoutCurrency, 10);
   };
@@ -72,9 +70,13 @@ const Product = () => {
     } else {
       let sortedData;
       if (value === 'priceLowToHigh') {
-        sortedData = [...cardData].sort((a, b) => extractPriceValue(a.price) - extractPriceValue(b.price));
+        sortedData = [...cardData].sort(
+          (a, b) => extractPriceValue(a.price) - extractPriceValue(b.price)
+        );
       } else if (value === 'priceHighToLow') {
-        sortedData = [...cardData].sort((a, b) => extractPriceValue(b.price) - extractPriceValue(a.price));
+        sortedData = [...cardData].sort(
+          (a, b) => extractPriceValue(b.price) - extractPriceValue(a.price)
+        );
       } else if (value === 'rating') {
         sortedData = [...cardData].sort((a, b) => b.averageRatings - a.averageRatings);
       }
@@ -92,8 +94,8 @@ const Product = () => {
       await getData();
     })();
   }, []);
-  
-  const handleShare = (product_Id) => {
+
+  const handleShare = product_Id => {
     const base_url = window.location.origin;
     const url = `${base_url}/products/${product_Id}`;
     navigator.clipboard
@@ -101,19 +103,22 @@ const Product = () => {
       .then(() => {
         notification.success({
           message: 'Success',
-          description: "Product Link copied to clipboard!"
+          description: 'Product Link copied to clipboard!',
         });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log('Error copying to clipboard:', error);
       });
   };
-  
 
   return (
     <Layout style={{ flex: 1, overflow: 'hidden' }}>
-      <GlobalHeader userName = {userName} title={'Products'} handleSearchResults={handleSearchResults}/>
-      <div style={{ position: 'absolute', top: 70, right: 24, zIndex: 1 ,margin: '10px'}}>
+      <GlobalHeader
+        userName={userName}
+        title={'Products'}
+        handleSearchResults={handleSearchResults}
+      />
+      <div style={{ position: 'absolute', top: 70, right: 24, zIndex: 1, margin: '10px' }}>
         <Select
           value={sortingOption}
           style={{ width: 200 }}
@@ -126,7 +131,7 @@ const Product = () => {
           <Option value="rating">Rating: Highly Rated</Option>
         </Select>
       </div>
-      <br/>
+      <br />
       <Content style={{ padding: '24px', overflow: 'auto' }}>
         <div className="user-dummy-container_p">
           <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
@@ -145,43 +150,44 @@ const Product = () => {
                   hoverable
                   style={{ width: '100%' }}
                   cover={
-                    
                     <div
-      style={{
-        width: '100%',
-        height: '250px', 
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        overflow: 'hidden',
-      }}
-    >
-      <a onClick={() => productDetails(e._id)}>
-        <img
-          className="card-image"
-          alt="example"
-          src={e.image}
-          style={{
-            width: '100%',
-            objectFit: 'cover', 
-            maxHeight: '100%', 
-          }}
-        />
-      </a>
-    </div>
+                      style={{
+                        width: '100%',
+                        height: '250px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        overflow: 'hidden',
+                      }}
+                    >
+                      <a onClick={() => productDetails(e._id)}>
+                        <img
+                          className="card-image"
+                          alt="example"
+                          src={e.image}
+                          style={{
+                            width: '100%',
+                            objectFit: 'cover',
+                            maxHeight: '100%',
+                          }}
+                        />
+                      </a>
+                    </div>
                   }
                   actions={[
-                    <div onClick={() => handleAddLike(e._id)} style={{ cursor: 'pointer' }}>
-                    <Tooltip placement="bottom" title={<span>Like</span>}>
-                      <LikeOutlined
-                        key="like"
-                        onClick={() => handleAddLike(e._id)}
-                        style={{ color: e.isLiked ? 'blue' : 'inherit' }}
-                      />
-                      <span className="like-count">({e.isLikedTotal})</span>
-                    </Tooltip>
-                    </div>
-                    ,
+                    <div
+                      role="presentation"
+                      onClick={() => handleAddLike(e._id)}
+                      style={{ cursor: 'pointer' }}
+                    >
+                      <Tooltip placement="bottom" title={<span>Like</span>}>
+                        <LikeOutlined
+                          key="like"
+                          style={{ color: e.isLiked ? 'blue' : 'inherit' }}
+                        />
+                        <span className="like-count">({e.isLikedTotal})</span>
+                      </Tooltip>
+                    </div>,
                     <Tooltip placement="bottom" title={<span>Share</span>}>
                       <ShareAltOutlined key="share" onClick={() => handleShare(e._id)} />
                     </Tooltip>,
@@ -220,5 +226,3 @@ const Product = () => {
 };
 
 export default Product;
-
-
